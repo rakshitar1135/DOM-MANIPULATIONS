9b862e70c5cdf925f0cebe904d71d5dd683967cd
rakshitar1135 <rakshitaraju2000@gmail.com>
2022-10-10 20:02:31 +0530

    slight

--- raksh/task5.js
+++ raksh/task5.js
@@ -1,11 +1,13 @@
-var li=document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent='Hello2';
-li[0].style.fontWeight='bold';
-li[1].style.fontWeight='bold';
-li[2].style.fontWeight='bold';
-li[3].style.fontWeight='bold';
-li[2].style.backgroundColor='green';
-for(var i=0;i<li.length;i++)
-li[i].style.backgroundColor='#f4f4f4';
+//var li=document.getElementsByTagName('li');
+//console.log(li);
+//console.log(li[1]);
+//li[1].textContent='Hello2';
+//li[0].style.fontWeight='bold';
+//li[1].style.fontWeight='bold';
+//li[2].style.fontWeight='bold';
+//li[3].style.fontWeight='bold';
+//li[2].style.backgroundColor='green';
+//for(var i=0;i<li.length;i++)
+//li[i].style.backgroundColor='#f4f4f4';
+var header=document.querySelector('#main-header');
+header.style.borderBottom='solid 4px #ccc';
