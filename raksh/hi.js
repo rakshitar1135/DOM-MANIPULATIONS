4332a4025c27ed1e5dd9d77de0b3ad3aaff9eaa7
rakshitar1135 <rakshitaraju2000@gmail.com>
2022-10-10 15:38:33 +0530

    make changes1

---
+++ raksh/hi.js
@@ -0,0 +1,14 @@
+//console.dir(document);
+//console.log(document.URL);
+//console.log(document.title);
+//console.log(document.all);
+//console.log(document.all);
+//document.all[10].textcontent='hello';
+//console.log(document.images);
+var headerTitle=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.textContent='HELLO';
+//headerTitle.innerText='GOODBYE;'
+//headertitle.innerHTML=<h3>Hello</h3>
+header.style.borderBottom='solid 3px #000'
