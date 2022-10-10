1b5e350f860c406a0c5c8f79995131d64301c18a
rakshitar1135 <rakshitaraju2000@gmail.com>
2022-10-10 21:10:06 +0530

    changesabit

--- raksh/task5.js
+++ raksh/task5.js
@@ -9,15 +9,25 @@
 //li[2].style.backgroundColor='green';
 //for(var i=0;i<li.length;i++)
 //li[i].style.backgroundColor='#f4f4f4';
-var header=document.querySelector('#main-header');
-header.style.borderBottom='solid 4px #ccc';
-var input=document.querySelector('input')
-input.value='HELLO WORLD'
-var Submit=document.querySelector('input[type="submit"]');
-Submit.value="SEND";
-var item=document.querySelector('.list-group-item');
-item.style.color='red';
-var Lastitem=document.querySelector('.list-group-item:last-Child')
-Lastitem.style.color='green';
-var seconditem=document.querySelector('.list-group-item:nth-child[2]')
-seconditem.style.color='coral';
+//var header=document.querySelector('#main-header');
+//header.style.borderBottom='solid 4px #ccc';
+//var input=document.querySelector('input')
+//input.value='HELLO WORLD'
+//var Submit=document.querySelector('input[type="submit"]');
+//Submit.value="SEND";
+//var item=document.querySelector('.list-group-item');
+//item.style.color='red';
+//var Lastitem=document.querySelector('.list-group-item:last-Child')
+//Lastitem.style.display='none';
+//var secondItem=document.querySelector('.list-group-item:nth-child(2)')
+//secondItem.style.backgroundColor='green';
+var titles=document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent='Hello';
+var odd=document.querySelectorAll('li:nth-child(odd)');
+var even=document.querySelectorAll('li:nth-child(even)');
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor='#f4f4f4';
+    even[i].style.backgroundColor='#ccc';
+}
